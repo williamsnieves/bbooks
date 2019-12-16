a17f643861431dc0c16a047fbab5f278cd45cf3d
willians nieves <willians.nieves@i-surance.es>
2019-12-16 10:12:30 +0100

    master - WIP - updated router

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./styles/App.css";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
-import { Router } from "@reach/router";
+import { Router, Redirect } from "@reach/router";
 
 function App() {
   return (
     <div className="App" data-testid="main-container">
       <Router>
+      	<Redirect noThrow from="/" to="login"/>
         <Login path="/login" />
         <Dashboard path="/dashboard" />
       </Router>
